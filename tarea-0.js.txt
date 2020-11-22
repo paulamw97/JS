8c5088d2671d37847da2c981b49bc408ca04f16e
paula <72078696+paulamw97@users.noreply.github.com>
2020-11-22 11:40:53 +0100

    Add files via upload

---
+++ tarea-0.js.txt
@@ -0,0 +1,5 @@
+/* 
+fichero con codigo javascript
+alumno:
+fecha:
+*/
---
+++ tarea-1.js
@@ -0,0 +1,10 @@
+/* 
+fichero con codigo javascript
+alumno:
+fecha:
+*/
+var cad=prompt("ingrese su nombre:","");
+var cad2=prompt("ingrese su edad:","");
+alert(cad+ + cad2 + " esa es el nombre y la edad que ha ingresado");
+var cad= cad + " gracias por decirnos su edad ";
+document.getElementById("ubicacion1").innerHTML = cad;
